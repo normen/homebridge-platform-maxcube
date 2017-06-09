9f808ebf00c78466c74048d96ed32ef21fc2c491
normen <normen667@gmail.com>
2017-06-09 16:49:59 +0200

    Fix connection issues, add wall theormostat
    - Fix plugin stalling HomeBridge when cube can't be found on startup
    - Added option to use wall thermostat devices via allow_wall_thermostat config option

--- index.js
+++ index.js
@@ -1,57 +1,51 @@
 var MaxCube = require('maxcube');
 var Thermostat = require('./thermostat');
 
-
 /** Sample platform outline
  *  based on Sonos platform
  */
-
 function MaxCubePlatform(log, config){
-    this.log = log;
-    this.config = config;
-    this.refreshed = false;
-    if (this.config) {
-      //this.log('Connecting to MaxCube')
-      this.cube = new MaxCube(this.config.ip, this.config.port);
-    }
+  this.log = log;
+  this.config = config;
+  this.refreshed = false;
+  if (this.config) {
+    this.cube = new MaxCube(this.config.ip, this.config.port);
+  }
 };
 MaxCubePlatform.prototype = {
-    accessories: function(callback) {
-      //this.log("Fetching maxCube devices.");
-      var that = this;
-      this.cube.maxCubeLowLevel.on('error', function (error) {
-          that.log("Max! Cube Error:", error);
-      });
-      this.cube.on('connected', function () {
-        //that.log('Connected');
-
+  accessories: function(callback) {
+    var that = this;
+    this.cube.maxCubeLowLevel.on('error', function (error) {
+      that.log("Max! Cube Error:", error);
+      if(!this.refreshed){
+        // We didn't connect yet and got an error,
+        // probably the Cube couldn't be reached,
+        // fulfill the callback so HomeBridge can initialize.
         var myAccessories = [];
-        that.cube.getDeviceStatus().then(function (devices) {
-          if (this.refreshed){
-            //that.log('Already refreshed');
-            return;
+        callback(myAccessories);
+      }
+    });
+    this.cube.on('connected', function () {
+      var myAccessories = [];
+      if (this.refreshed) return;
+      that.cube.getDeviceStatus().then(function (devices) {
+        this.refreshed = true;
+        devices.forEach(function (device) {
+          var deviceInfo = that.cube.getDeviceInfo(device.rf_address);
+          var wall = that.config.allow_wall_thermostat && (deviceInfo.device_type == 3);
+          if (deviceInfo.device_type == 1 || deviceInfo.device_type == 2 || wall) {
+            myAccessories.push(new Thermostat(that.log, that.config, device, deviceInfo, that.cube, Service, Characteristic));
           }
-          this.refreshed = true;
-          devices.forEach(function (device) {
-            //that.log('gegting device info for '+ device.rf_address)
-            var deviceInfo = that.cube.getDeviceInfo(device.rf_address);
-
-            if (deviceInfo.device_type == 1||deviceInfo.device_type == 2) {
-              //that.log('registering device', deviceInfo.device_name+" ("+deviceInfo.room_name+")")
-              myAccessories.push(new Thermostat(that.log, that.config, device, deviceInfo, that.cube, Service, Characteristic));
-            }
-          });
-          //that.cube.close();
-          callback(myAccessories);
         });
+        callback(myAccessories);
       });
-    }
+    });
+  }
 };
 
 var Service;
 var Characteristic;
 
-// more
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
